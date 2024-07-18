bf6037e72d97f8b9f02fd88953b86344d6f0ecbf
Alberto F. Borella <113259191+albertoborella@users.noreply.github.com>
2024-07-18 18:02:41 -0300

    Add files via upload

--- ejercicios_practica/src/App.js
+++ ejercicios_practica/src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import { NameList } from './components/NameList';
+import { Counter } from './components/Counter';
+import { NoteApp } from './components/NoteApp';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        AQUÍ AGREGUE SUS COMPONENTES
+        <NameList />
+        <Counter />
+        <NoteApp />
       </header>
     </div>
   );
